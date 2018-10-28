951499b6d8d27d6ac052fa5ba70c8ebaf29c1590
Guillaume Libersat <glibersat@sigill.org>
2018-10-28 20:30:28 +0100

    - add coap support  - add watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,40 @@
+//#define USE_LCD
+#define USE_COAP
+
 #include <Arduino.h>
+#include <avr/wdt.h>
+
 #include <Automaton.h>
 
-#include <menu.h>
+#ifdef USE_LCD
+#include <menu.hpp>
 #include <menuIO/chainStream.h>
 #include <menuIO/U8x8Out.h>
+#endif
+
+#ifdef USE_COAP
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <ArduinoJson.h>
+
+#include <coap.h>
+
+#endif
+
 
 #include "Atm_volume_sensor.hpp"
 
-using namespace Menu;
+#ifdef USE_COAP
+// Ethernet setup
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xFE, 0x40 };
+
+// UDP and CoAP class
+EthernetUDP Udp;
+Coap coap(Udp);
+
+#endif
+
+#ifdef USE_LCD
 
 // pin definitions
 #define OLED_CS_PIN 13
@@ -16,12 +43,14 @@ using namespace Menu;
 #define OLED_MOSI_PIN 10
 #define OLED_CLK_PIN 9
 
+#define MAX_DEPTH 2
+
+#endif
+
 #define WATER_IN_RELAY_PIN 2
 #define WATER_OUT_RELAY_PIN 3
 
 #define BUTTON_PIN 8
-
-#define MAX_DEPTH 2
 
 Atm_volume_sensor volume_sensor;
 Atm_led water_in_relay, water_out_relay;
@@ -34,11 +63,77 @@ Atm_bit filling, transferring;
 int fill_target = 0;
 int tx_amount = 0;
 
+// Average buffer for sensor
+uint16_t avgbuffer[16];
+
 const int max_volume = 9000; // dL
 
 enum error_no {X};
 
+#ifdef USE_COAP
 
+// CoAP server endpoint URL
+callback callback_status(CoapPacket &packet, IPAddress ip, int port) {
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+  root[String("volume")] = volume_sensor.state();
+  root[String("filling")] = filling.state();
+  root[String("filling_target")] = fill_target;
+  root[String("transferring")] = transferring.state();
+  root[String("transferring_amount")] = tx_amount;
+
+  String answer_json;
+  root.printTo(answer_json);
+
+  coap.sendResponse(ip, port, packet.messageid, (char*)answer_json.c_str(), answer_json.length(), COAP_CONTENT, COAP_APPLICATION_JSON ,NULL, 0);
+}
+
+// CoAP server endpoint URL
+callback callback_fill(CoapPacket &packet, IPAddress ip, int port) {
+
+  char p[packet.payloadlen + 1];
+  memcpy(p, packet.payload, packet.payloadlen);
+  p[packet.payloadlen] = NULL;
+
+  String message(p);
+
+  int fill_to = message.toInt();
+  if (fill_to > 0 and fill_to < max_volume) {
+    fill_target = fill_to;
+    filling.on();
+    coap.sendResponse(ip, port, packet.messageid, NULL, 0, COAP_VALID, COAP_APPLICATION_JSON, NULL, 0);
+  } else {
+    fill_target = 0;
+    filling.off();
+    coap.sendResponse(ip, port, packet.messageid, NULL, 0, COAP_NOT_ACCEPTABLE, COAP_APPLICATION_JSON, NULL, 0);
+  }
+}
+
+
+callback callback_transfer(CoapPacket &packet, IPAddress ip, int port) {
+
+  char p[packet.payloadlen + 1];
+  memcpy(p, packet.payload, packet.payloadlen);
+  p[packet.payloadlen] = NULL;
+
+  String message(p);
+
+  int transfer_amount = message.toInt();
+  if (transfer_amount > 0 and transfer_amount < volume_sensor.state()) {
+    tx_amount = transfer_amount;
+    transferring.on();
+    coap.sendResponse(ip, port, packet.messageid, NULL, 0, COAP_VALID, COAP_APPLICATION_JSON, NULL, 0);
+  } else {
+    tx_amount = transfer_amount;
+    transferring.off();
+    coap.sendResponse(ip, port, packet.messageid, NULL, 0, COAP_NOT_ACCEPTABLE, COAP_APPLICATION_JSON, NULL, 0);
+  }
+}
+
+#endif
+
+#ifdef USE_LCD
 // Display
 U8X8_SSD1306_128X32_UNIVISION_4W_SW_SPI u8x8(OLED_CLK_PIN, OLED_MOSI_PIN, OLED_CS_PIN, OLED_DC_PIN, OLED_RST_PIN);
 
@@ -77,8 +172,6 @@ MENU(mainMenu, "HLT", doNothing, noEvent, wrapStyle,
 );
 
 NAVROOT(nav, mainMenu, MAX_DEPTH, in, out); //the navigation root object
-
-uint16_t avgbuffer[16];
 
 // Notify we need to update display
 void request_update_display(int idx, int v, int up) {
@@ -138,19 +231,46 @@ result draw_volume_big(menuOut& o, idleEvent event) {
   return proceed;
 }
 
+#endif // USE_LCD
+
 void setup() {
+  wdt_disable();
+
   // Start serial communication and set baud rate = 9600
   Serial.begin(9600);
 
+#ifdef USE_LCD
   // OLED Display
   u8x8.begin();
   u8x8.setPowerSave(0);
   u8x8.setFont(u8x8_font_saikyosansbold8_u);
+#endif
+
+#ifdef USE_COAP
+  // Start the Ethernet connection and the server
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    // Ethernet.begin(mac, ip);
+  }
+
+  // CoAP callbacks
+  coap.server(callback_status, "status");
+  coap.server(callback_fill, "fill");
+  coap.server(callback_transfer, "transfer");
+
+  coap.start();
+
+#endif // USE_COAP
 
   // Sensor reading
   volume_sensor.begin(10)
     .average(avgbuffer, sizeof(avgbuffer))
-    .onChange(request_update_display);
+#ifdef USE_LCD
+    .onChange(request_update_display)
+#endif
+    ;
 
   // Flags
   filling.begin()
@@ -180,7 +300,7 @@ void setup() {
   water_in_relay.begin(WATER_IN_RELAY_PIN, false).off();
   water_out_relay.begin(WATER_OUT_RELAY_PIN, true).off();
 
-
+#ifdef USE_LCD
   // Trigger button
   button.begin(BUTTON_PIN)
     .debounce(100)
@@ -204,10 +324,27 @@ void setup() {
 
   nav.idleTask = draw_volume_big;
   nav.idleOn(draw_volume_big);
+
+#endif // USE_LCD
+
+  //  Re-enable watchdog
+  delay(1000L);
+  wdt_enable(WDTO_4S);
 }
 
 
 void loop() {
+#ifdef USE_LCD
   nav.doOutput();
+#endif // USE_LCD
+
+#ifdef USE_COAP
+  coap.loop();
+#endif // USE_COAP
+
+  // main logic
   automaton.run();
+
+  // Reset watchdog
+  wdt_reset();
 }
